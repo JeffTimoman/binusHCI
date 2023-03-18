3645857f4edf6d76d2a748464382ead9154c0dce
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2023-03-18 17:23:01 +0700

    Daily Update

--- FINAL LAB/assets/script/mondstadt.js
+++ FINAL LAB/assets/script/mondstadt.js
@@ -18,6 +18,8 @@ let sliderBorders = document.querySelectorAll(".sliderBorder");
 let charName = document.getElementById("charName");
 let charVision = document.getElementById("charVision");
 let charNameList = ["JEAN", "LISA", "VENTI", "KLEE"]
+let charDescList = ["As the Acting Grand Master of the Knights, Jean has always been devoted to her duties and maintaining peace in Mondstadt.", "She is an intellectual witch who can never get enough naps. As the Librarian of the Knights of Favonius, Lisa is smart in that she always knows exactly what to do with whatever troubles her.", "The anemo god from mondstadt, the great barbatos, taking the form of a bard named Venti.", "Knights of Favonius Spark Knight! Forever witha bang and a flash! —And then disappearing from the stern gaze of Acting Grand Master Jean."]
+let charDesc = document.getElementById("charDesc")
 let charBigImg = document.getElementById("charBigImg");
 
 for(let i = 0; i < charOptions.length; i++){
@@ -30,15 +32,24 @@ for(let i = 0; i < charOptions.length; i++){
         sliderBorders[i].src = "../../assets/img/characters/slider-active.png"
         charBigImg.src = `../../assets/img/characters/mondstadt/big-${i+1}.png`
         charVision.src = `../../assets/img/characters/mondstadt/vision-${i+1}.png`
+        charDesc.innerHTML = charDescList[i]
         charName.innerHTML = charNameList[i]
         charName.style.animation = "fadeIn 1s";
         charVision.style.animation = "fadeIn 1s";
         charBigImg.style.animation = "fadeIn 1s";
+        charDesc.style.animation = "fadeIn 1s";
         setTimeout(() => {
             charName.style.animation = "";
             charVision.style.animation = "";
             charBigImg.style.animation = "";
+            charDesc.style.animation = "";
         }, 1000);
+
+        // windows scroll to top make the scroll smooth
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     })
 }
 
--- FINAL LAB/assets/script/navFooter.js
+++ FINAL LAB/assets/script/navFooter.js
@@ -6,3 +6,31 @@ window.onscroll = () => {
         document.getElementById("navBar").style.background = "rgba(48, 47, 41, 0.6)"
     }
 }   
+
+
+let hamOpen = document.querySelector(".open");
+let hamClose = document.querySelector(".close");
+let navside = document.querySelector(".navBarSide");
+
+hamOpen.onclick = () => {
+    hamOpen.classList.toggle('active')
+    hamClose.classList.toggle('active')
+    navside.classList.toggle('active')
+    hamOpen.style.animation = "fadeIn 0.5s";
+    hamClose.style.animation = "fadeIn 0.5s";
+    setTimeout(() => {
+        hamClose.style.animation = "";
+        hamOpen.style.animation = "";
+    }, 500);
+}
+hamClose.onclick = () => {
+    hamOpen.classList.toggle('active')
+    hamClose.classList.toggle('active')
+    navside.classList.toggle('active')
+    hamOpen.style.animation = "fadeIn 0.5s";
+    hamClose.style.animation = "fadeIn 0.5s";
+    setTimeout(() => {
+        hamClose.style.animation = "";
+        hamOpen.style.animation = "";
+    }, 500);
+}
